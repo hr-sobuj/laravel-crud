5ce231b47eb71ea8a3cc41acfecce9975d8b630b
Habibur Rahman Sobuj <sobujhd@gmail.com>
2024-05-08 15:46:34 +0600

    initial configaration

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class,'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
