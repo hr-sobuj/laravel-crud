5faf2ac1bbc0a01ca331595e0ebc6085933b0df1
Habibur Rahman Sobuj <sobujhd@gmail.com>
2024-05-09 20:36:14 +0600

    create some new components

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,18 +1,19 @@
 @props(['isLoggedIn'])
 
-<nav class="flex justify-between items-center">
-    <a href="{{ route('home') }}" class="flex space-x-2 justify-center items-center">
-        <img src="https://laravel.com/img/logomark.min.svg" alt="Logo" class="h-8">
-        <img src="https://laravel.com/img/logotype.min.svg" alt="Laravel Logo" class="h-8">
-    </a>
-    <div class="flex space-x-1">
-        @if ($isLoggedIn)
-            <a href="{{ route('posts.index') }}" class="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded">Dashboard</a>
-        @else
-            <a href="{{ route('login') }}" class="bg-lime-500 hover:bg-lime-600 text-white font-bold py-2 px-4 rounded">Log in</a>
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded">Register</a>
+<header class="bg-white dark:bg-gray-900">
+    <nav class="flex justify-between items-center container mx-auto px-4 py-6">
+        <a href="{{ route('home') }}" class="flex space-x-2 justify-center items-center">
+            <img src="https://laravel.com/img/logotype.min.svg" alt="Laravel Logo" class="h-8">
+        </a>
+        <div class="flex space-x-1">
+            @if ($isLoggedIn)
+                <a href="{{ route('posts.index') }}" class="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded dark:bg-teal-800 dark:hover:bg-teal-700">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}" class="bg-lime-500 hover:bg-lime-600 text-white font-bold py-2 px-4 rounded dark:bg-lime-800 dark:hover:bg-lime-700">Log in</a>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded dark:bg-indigo-800 dark:hover:bg-indigo-700">Register</a>
+                @endif
             @endif
-        @endif
-    </div>
-</nav>
+        </div>
+    </nav>
+</header>
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="dark">
 
 <head>
     <meta charset="utf-8">
@@ -8,16 +8,18 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="font-sans antialiased bg-gray-100">
+<body class="font-sans antialiased bg-gray-100 dark:bg-gray-800 dark:text-white">
+
+    <x-navbar :isLoggedIn="auth()->check()" />
+
     <div class="container mx-auto px-4 py-8">
 
-        <x-navbar :isLoggedIn="auth()->check()" />
-
-        <x-filter />
+        <x-filter action="{{ route('home') }}" />
 
         <div class="mt-8">
             @yield('content')
         </div>
+
     </div>
 </body>
 
