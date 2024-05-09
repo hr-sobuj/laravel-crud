d29be80d217e9852851ca0efc6297379a801500d
Habibur Rahman Sobuj <sobujhd@gmail.com>
2024-05-09 17:42:47 +0600

    update filter components

--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -14,6 +14,6 @@
     </div>
     <div class="mt-4">
         <button type="submit"
-            class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 dark:bg-red-600 dark:hover:bg-red-700">Filter</button>
+            class="inline-flex items-center px-6 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gray-500 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 dark:bg-gray-600 dark:hover:bg-gray-700">Filter</button>
     </div>
 </form>
